e8dde51bb5bd582d0a18e6134a699836b82b2a64
suntao <suntao@sunniwell.net>
2020-09-10 09:31:22 +0800

    aaaa

---
+++ project1/src/net/sunniwell/git/FirstClass.java
@@ -0,0 +1,8 @@
+package net.sunniwell.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hahahhahahah ");
+        System.out.println("tijiaoaaaaaa");
+    }
+}
